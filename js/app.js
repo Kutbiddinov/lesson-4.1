4022fb438e9c600094f2f9d5b217954bca125aad
TheHanry <samandarqutbiddinov403@gmail.com>
2023-03-25 16:59:36 +0500

    main

--- js/app.js
+++ js/app.js
@@ -7,45 +7,30 @@ function renderFunc(array, element) {
     let newImg = creatEl("img");
     let newh2 = creatEl("h2");
     let newP = creatEl("p");
-    let newGenreList = creatEl("ul");
+    // let newGenreList = creatEl("ul");
     let newBtn = creatEl("button");
 
     setAtt(newImg, "src", film.film_image);
-    setAtt(newImg, "style", "width: 400px");
+    setAtt(newImg, "style", "width: 244px;");
     setAtt(newBtn, "type", "button");
-    setAtt(newLi, "class", "item");
-    setAtt(newImg, "class", "img");
-    setAtt(newh2, "class", "h2");
-    setAtt(newP, "class", "p");
-    setAtt(newGenreList, "class", "genrelist");
-    setAtt(newBtn, "class", "btn");
+    setAtt(newLi, "style", "border: 1px solid; width: 244px; height: 486px;");
+    setAtt(newh2, "style", "width: 244px; margin: 0px 0px 10px 10px; font-family: 'Roboto'; font-style: normal; font-weight: 500; font-size: 22px; line-height: 26px; color: #FFFFFF;");
+    setAtt(newP, "style", " margin: 0px 0px 10px 10px; font-family: 'Roboto'; font-style: normal; font-weight: 400; font-size: 20px; line-height: 23px; color: #FFFFFF;");
+    // setAtt(newGenreList, "class", "genrelist");
 
     newh2.textContent = film.title;
-    newP.textContent = "Genre:";
+    newP.textContent = film.description;
     newBtn.textContent = "Show more";
 
-    film.film_genre.forEach((genre) => {
-      let newGenreLi = creatEl("li");
-      newGenreLi.textContent = genre;
-      newGenreList.append(newGenreLi);
-    });
+    // film.film_genre.forEach((genre) => {
+    //   let newGenreLi = creatEl("li");
+    //   newGenreLi.textContent = genre;
+    //   newGenreList.append(newGenreLi);
+    // });
 
-    newLi.append(newImg, newh2, newP, newGenreList, newBtn);
+    newLi.append(newImg, newh2, newP, newBtn);
 
     element.append(newLi);
   });
 }
 renderFunc(filmData, elList);
-
-let fruitArr = ["olma", "anor", "shoftoli", "mandarin"];
-
-function sortGenre(array) {
-  array.forEach((film) => {
-    film.film_genre.forEach((genre) =>
-      genreArray.includes(genre) ? null : genreArray.push(genre)
-    );
-  });
-}
-
-// sortGenre(filmData);
-// console.log(genreArray);
